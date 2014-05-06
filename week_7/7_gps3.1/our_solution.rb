2435265e48a7101e616bb1f96c523cb6640ea863
mceoin <eoinmcmillan@gmail.com>
2014-05-05 21:45:26 -0700

    Working grocery list
    Driver code & refactor to come

--- week_7/7_gps3.1/our_solution.rb
+++ week_7/7_gps3.1/our_solution.rb
@@ -1,34 +1,78 @@
-# U3.W7: Build an Electronic Grocery List!
- 
-# Your full names:
-# 1.
-# 2.
- 
-# User Stories (As many as you want. Delete the statements you don't need)
-# As a user, I want to
-# As a user, I want to
-# As a user, I want to
-# As a user, I want to
-# As a user, I want to
- 
-# Pseudocode
-# 
-# 
-# 
-# 
-# 
-# 
- 
- 
-# Your fabulous code goes here....
+# U3.W7: GPS 3.1 Create an Electronic Grocery List
+# Learning Competencies:
+# Implement a basic Ruby class and identify when to use instance variables
+# Identify the attributes and methods of a class based on user stories
+# Identify tradeoffs between simple OO design patterns
+# Summary
+# In this challenge, you will create an electronic grocery list command-line application. Show off the object-oriented design principles you learned last week. You should spend time discussing tradeoffs with different designs. This is your grocery list, so get creative and have fun, but also make it great code!
+
+# Release 0: User Stories
+# With your partner, talk about what you would like to do with your grocery list. Start each statement with "As a user, I want to ..." These are what we call "user stories."
+
+# As a user, I'd like to be able to make a shopping list, no harder than pencil & paper process
+# As a user, I'd like to be able to add items to this shopping list, stating the quantity of each item
+# As a user, I'd like to be able to remove items from my shopping list
+# As a user, I'd like to be able to check what items remain in my shopping list (items that are yet to be purchased)
+
+# Release 1: Pseudocode
+# What objects, classes, and methods will you need to do each of the things you identified in your user stories?
+# Create a class called shopping list that takes an argument
+# Initialize shoppingList.new( )
+# Create a method add item that takes an item, and quantity. e.g. item("apple"=> 5) --- otherwise known as a hash
+# Create a method that prints all items in the shopping list, and quantity: output e..g apple x 5. If there are no items, print "no items left!"
+# Create a method that allows you to remove items from the shopping list by quantity (e.g. "apple=> 3" )
+
+# Release 2: Write Driver Code
+# Translate your pseudocode into driver code. Write driver code at the bottom (to call the method on the object).
+
+# Release 3: Initial Solution
 
 
 
+class ShoppingList
+    def initialize 
+        @list = { }
+    end
+    
+    def add(item, qty)
+        @list[item]= qty
+    end
+
+    def remove(item, qty)
+   	quantity = @list[item]
+    new_quantity = quantity - qty
+    @list[item]=new_quantity
+    end
+
+    def print_list
+    	puts "-----------------------"
+    	puts "Shopping List:"
+    	puts "-----------------------"
+    	@list.each_pair {|key, value| puts "#{key} x #{value}"}
+    	puts "-----------------------"
+    end
+    
+end
+    grocery_list = ShoppingList.new
+
+    grocery_list.add("apple", 5)
+    grocery_list.remove("apple", 5)
+    grocery_list.add("tomato", 7)
+    grocery_list.add("cherry", 100)
+    grocery_list.add("pie", 6)
+	
+	grocery_list.print_list
+
+#check for "string" key value in our grocery_list
+#if key match , get value
+#new value - old value = updated value
 
 
+# Release 4: Refactor your solution
+# With your partner, review the code. Is it DRY? Are the names good?
 
+# Release 5: Reflect
+# Copy your code from stypi into the our_solution.rb file. Then complete a reflection in the designated section.
 
-# DRIVER CODE GOES HERE. 
- 
- 
- 
+# Release 6: Sync your changes to Github!
+# Release 6: Submit Feedback.
--- week_8_and_9/4_Ruby/oo_basics/my_solution.rb
+++ week_8_and_9/4_Ruby/oo_basics/my_solution.rb
@@ -12,23 +12,13 @@
 class Student
   attr_accessor :scores, :first_name
 
-  def initialize(args)   #Use named arguments!
-    #your code here
-  end
+  def initialize(string)   #Use named arguments!
+    :first_name
+    :scores
+    Alex = Student.new("Alex",[100,100,100,0,100])
+    end
 end
 
-# def findStudent(array)
-# 	array.each do |i| 
-		
-
-
-# function findIndex(values, target) {
-#   for(var i = 0; i < values.length; ++i){
-#     if (values[i] == target) { return i; }
-#   }
-#   return -1;
-# }
-# findIndex([7, 3, 6, 1, 0], 6)
 
 # 4. Refactored Solution
 
@@ -40,21 +30,21 @@ end
 # 1. DRIVER TESTS GO BELOW THIS LINE
 # Tests for release 0:
 
-p students[0].first_name == "Alex"
-p students[0].scores.length == 5
-p students[0].scores[0] == students[0].scores[4]
-p students[0].scores[3] == 0
+# p students[0].first_name == "Alex"
+# p students[0].scores.length == 5
+# p students[0].scores[0] == students[0].scores[4]
+# p students[0].scores[3] == 0
 
 
-# Tests for release 1:
+# # Tests for release 1:
 
-p students[0].average == 80
-p students[0].letter_grade == 'B'
+# p students[0].average == 80
+# p students[0].letter_grade == 'B'
 
-# Tests for release 2:
+# # Tests for release 2:
 
-p linear_search(students, "Alex") == 0
-p linear_search(students, "NOT A STUDENT") == -1
+# p linear_search(students, "Alex") == 0
+# p linear_search(students, "NOT A STUDENT") == -1
 
 
 
